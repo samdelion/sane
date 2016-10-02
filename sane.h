b29207e90ab64acd5b633c451adb09c78afba9e6
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2016-10-02 21:29:43 +0800

    Cleanup header files
    Cleanup header files so that commenting style is consistent across
    header files.

--- sane.h
+++ sane.h
@@ -1,13 +1,10 @@
 struct command_t;
 
 ////////////////////////////////////////////////////////////////////////////////
-// Execute the commands found in the command array.
-//
-// @param   numCommands   int, number of commands in the command array.
-// @param   commands      command_t, command array.
-// @return                int, 1 for success, 0 in case of error or quit.
+/// Execute the commands found in the command array.
+///
+/// @param   numCommands   int, number of commands in the command array.
+/// @param   commands      command_t, command array.
+/// @return                int, 1 for success, 0 in case of error or quit.
 ////////////////////////////////////////////////////////////////////////////////
 int sane_execute(int numCommands, command_t *commands);
-
-// void sane_loop(void);
-
