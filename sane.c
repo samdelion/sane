7ae4eb633b9a77c90b217a7425f235a255d02c84
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2016-10-03 10:31:40 +0800

    Minor cleanup

--- sane.c
+++ sane.c
@@ -15,19 +15,7 @@
 
 #include "command.h"
 
-////////////////////////////////////////////////////////////////////////////////
-/// For 'n' commands we need 'n - 1' pipe structures, which are made up of 2
-/// file
-/// descriptors each.
-////////////////////////////////////////////////////////////////////////////////
-#define MAX_NUM_PIPES ((MAX_NUM_COMMANDS - 1) * 2)
-
-// Where we store our pipes
-int sane_pipes[MAX_NUM_PIPES];
-// Number of pipes currently open
-unsigned int sane_numPipes = 0;
-
-// Shell built-in function declartions
+// Shell built-in function declarations
 /* int sane_cd(char **argv); */
 /* int sane_exit(char **argv); */
 int sane_help(char **argv);
@@ -36,7 +24,23 @@ int sane_help(char **argv);
 
 int sane_help(char **argv)
 {
-    printf("sane shell: Help granted.\n");
+    printf("     ___           ___           ___           ___     ");
+    printf("\n    /\\  \\         /\\  \\         /\\__\\         /\\  \\    ");
+    printf("\n   /::\\  \\       /::\\  \\       /::|  |       /::\\  \\   ");
+    printf(
+        "\n  /:/\\ \\  \\     /:/\\:\\  \\     /:|:|  |      /:/\\:\\  \\  ");
+    printf("\n _\\:\\~\\ \\  \\   /::\\~\\:\\  \\   /:/|:|  |__   /::\\~\\:\\  "
+           "\\ ");
+    printf("\n/\\ \\:\\ \\ \\__\\ /:/\\:\\ \\:\\__\\ /:/ |:| /\\__\\ /:/\\:\\ "
+           "\\:\\__\\");
+    printf("\n\\:\\ \\:\\ \\/__/ \\/__\\:\\/:/  / \\/__|:|/:/  / \\:\\~\\:\\ "
+           "\\/__/");
+    printf(
+        "\n \\:\\ \\:\\__\\        \\::/  /      |:/:/  /   \\:\\ \\:\\__\\  ");
+    printf("\n  \\:\\/:/  /        /:/  /       |::/  /     \\:\\ \\/__/  ");
+    printf("\n   \\::/  /        /:/  /        /:/  /       \\:\\__\\    ");
+    printf("\n    \\/__/         \\/__/         \\/__/         \\/__/   ");
+    printf("\n\n Samuel Evans-Powell and Nathan Gane shell\n");
 
     return 0;
 }
@@ -55,6 +59,18 @@ int sane_numBuiltins()
 {
     return sizeof(sane_builtinStr) / sizeof(char *);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+/// For 'n' commands we need 'n - 1' pipe structures, which are made up of 2
+/// file
+/// descriptors each.
+////////////////////////////////////////////////////////////////////////////////
+#define MAX_NUM_PIPES ((MAX_NUM_COMMANDS - 1) * 2)
+
+// Where we store our pipes
+int sane_pipes[MAX_NUM_PIPES];
+// Number of pipes currently open
+unsigned int sane_numPipes = 0;
 
 // Close all open pipes, sets sane_numPipes = 0
 void sane_pipesClose()
@@ -81,7 +97,6 @@ void sane_pipesCreate(unsigned int num)
 }
 
 // Stephen Brennan
-// return num executed
 pid_t sane_launch(command_t *command, int fdIn, int fdOut)
 {
     pid_t pid = -1;
@@ -149,61 +164,8 @@ pid_t sane_launch(command_t *command, int fdIn, int fdOut)
     return pid;
 }
 
-void sane_launchPipedSequence_r(int numCommands, command_t *commands)
-{
-    if (numCommands > 0 && commands != NULL) {
-        pid_t pid;
-
-        pid = fork();
-        if (pid == 0) {
-            // Child
-            if (execvp(commands[0].argv[0], commands[0].argv) == -1) {
-                perror("sane");
-            }
-            exit(EXIT_FAILURE);
-        } else if (pid > 0) {
-            // Check if more commands in sequence to execute
-            --numCommands;
-            if (numCommands >= 1) {
-                sane_launchPipedSequence_r(numCommands, ++commands);
-            }
-
-            // Parent
-            int status;
-
-            // Don't wait for background processes
-            if (strcmp(commands[0].sep, SEP_CON) != 0) {
-                // Wait for child process to finish
-                /* do { */
-                /*     waitpid(pid, &status, WUNTRACED); */
-                /* } while (!WIFEXITED(status) && !WIFSIGNALED(status)); */
-            } else {
-                // TODO: Add to jobs list
-                // If is a background process, print pid (like bash)
-                printf("%d\n", pid);
-            }
-        } else {
-            // Error
-            perror("sane");
-        }
-    }
-}
-
 int sane_execute(int numCommands, command_t *commands)
 {
-    /* // For each command */
-    /* for (int i = 0; i < numCommands; ++i) { */
-    /*     sane_launch(&commands[i]); */
-    /*     /\* if () *\/ */
-    /*     /\* for (int j = 0; j < sane_numBuiltins(); ++j) { *\/ */
-    /*     /\*     return (*sane_builtinFunc[j])(args); *\/ */
-    /*     /\* } *\/ */
-    /*     // If is built in, execute */
-    /*     // Else */
-    /*     //     fork */
-    /*     //       child execute command */
-    /*     //       parent wait if not background job */
-    /* } */
     int i = 0;
     while (i < numCommands) {
         if (strcmp(commands[i].sep, SEP_SEQ) == 0) {
@@ -265,24 +227,16 @@ int sane_execute(int numCommands, command_t *commands)
 
             for (int k = 0; k < numPipedCommands; ++k) {
                 if (k == 0) {
-                    /* printf("command: %d, fdIn: %d, fdOut: %d, \n", i + k, */
-                    /*        STDIN_FILENO, sane_pipes[1]); */
                     // First command in sequence:
                     //  - Use stdin for in, pipe for out (first write pipe)
                     sane_launch(&commands[i + k], STDIN_FILENO, sane_pipes[1]);
                 } else if (k == numPipedCommands - 1) {
-                    /* printf("command: %d, fdIn: %d, fdOut: %d, \n", i + k, */
-                    /*        sane_pipes[((sane_numPipes * 2) - 1) - 1], */
-                    /*        STDOUT_FILENO); */
                     // Last command in sequence:
                     //  - Use pipe (last read pipe) for in, stdout for out
                     sane_launch(&commands[i + k],
                                 sane_pipes[((sane_numPipes * 2) - 1) - 1],
                                 STDOUT_FILENO);
                 } else {
-                    /* printf("command: %d, fdIn: %d, fdOut: %d, \n", i + k, */
-                    /*        sane_pipes[(k - 1) * 2], sane_pipes[(k * 2) + 1]);
-                     */
                     // 'k'th command in sequence:
                     //  - Use pipe from previous command for in, pipe for this
                     //  command for out
--- token.h
+++ token.h
@@ -1,6 +1,13 @@
 #define MAX_NUM_TOKENS 1024
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Replaces each ' ' character in inputLine with a '\0' character.
+/// Given a string of characters provided in 'inputLine', splits the characters
+/// into a series of tokens and puts them in the output array 'token'.
+///
+/// @pre   'inputLine' is a NULL-terminated string.
+/// @pre   'token' is an array large enough to hold at least MAX_NUM_TOKENS
+///
+/// @param   inputLine   char *, string of characters to tokenise.
+/// @param   token       char *[], tokens out.
 ////////////////////////////////////////////////////////////////////////////////
 int tokenise(char *inputLine, char *token[]);
